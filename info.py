da879856a0173abe1ddf8d9eab87f7539fa818d3
QixianX <1037916520@qq.com>
2018-08-10 09:56:42 +0800

    Update info.py

--- info.py
+++ info.py
@@ -2,10 +2,11 @@ import re
 import urllib.request
 from selenium import webdriver
 import time
-import csv
+import hashlib
 
 
 def inf(html1):
+    nickname = re.findall('"nickname":"(.*?)"',  html1)  # 昵称
     gender = re.findall('"sex":(\d+)', html1)  # 性别
     age = re.findall('"age":(\d+)',  html1)  # 年龄
     birthday = re.findall('"birthday":"(.*?)"',  html1)  # 生日
@@ -29,7 +30,7 @@ def inf(html1):
 
     if '1' in gender:
         gender[0] = "男"
-    else:
+    elif '0' in gender:
         gender[0] = "女"
     if '4' in constellation:
         constellation[0] = "狮子座"
@@ -44,20 +45,39 @@ def inf(html1):
     else:
        marriage[0] = "其他"
     
-    dic={'性别':gender,'年龄':age,'生日':birthday,'出生年':birthyear,
-         ' 星座':constellation,
+    d={'昵称':nickname,'性别':gender,'年龄':age,'生日':birthday,'出生年':birthyear,
+         '星座':constellation,
          '血型': bloodtype,'婚姻状况': marriage,'居住地（国家）':living_country,
          '居住地（省份）':living_province,'居住地（城市）':living_city,
          '故乡（国家）':hometown_country,'故乡（省份）':hometown_provine,
          '故乡（城市）':hometown_city,'职业':career ,'公司名称':company,
          '公司地址（国家':company_country , '公司地址（省份)':company_province,
          '公司地址（城市）':company_city,'公司详细地址':company_address,}
-    print(dic)
-    dic1 = dic
-    csvFile = open('csvFile.csv','w', newline='') 
-    writer = csv.writer(csvFile)
-    for key in dic:
-        writer.writerow([key, dic[key]])
-    csvFile.close()
-
+    print(d)
+    #加密
+    a=[]
+    b=[]
+    #将字典中键和值循环取出添加到列表中
+    for i in d.keys():
+        a.append(i)
+        a.append(d[i])
+    print (a)
+    lens = len(a)
+    for i in range(lens):
+        if i%2 == 1:
+            x=a[i][0]
+            x = str(x)
+            b.append(x)
+        else:
+            x = a[i]
+            x = str(x)
+            b.append(x)
+    print(b)
+    c=("".join(b))
+    print(c)
+    s=hashlib.md5(c.encode(encoding='UTF-8')).hexdigest()
+    print(s)
+    f=open('jiami.txt','w')
+    f.write(s)
+    f.close()
     
